d103f4bf88cffa33b799b5aceb3636d36650f4af
isther <28251536@qq.com>
2022-03-29 09:41:17 +0800

    Added log

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/isther/web-base/conf"
+	log "github.com/sirupsen/logrus"
+)
 
 func main() {
-	fmt.Println("Hello")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	log.Warn("test")
+	r.Run(conf.Server.Listen)
 }
